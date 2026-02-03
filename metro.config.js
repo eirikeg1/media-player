6ba65b1785e543543a4ddcb6dac13ddb7e08056b
Eirik Eggset <e.eggset@gmail.com>
2026-02-03 21:20:54 +0100

    add rust backend

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure wasm is treated as an asset, not a source file
+config.resolver.assetExts.push('wasm');
+config.resolver.sourceExts = config.resolver.sourceExts.filter(ext => ext !== 'wasm');
+
 module.exports = withNativeWind(config, { input: './src/global.css', inlineRem: 16 });
